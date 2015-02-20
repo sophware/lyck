4c7aaf339de0e8499af7845b92dba43cf828d3f3
sophware <sophware@hotmail.com>
2015-02-20 00:49:28 -0500

    test old ycodes file
    new one seems not to be working

---
+++ rxvx700.h
@@ -0,0 +1,67 @@
+// todo: add Zone2 and Zone3
+const char* cmd[][2] = {
+    {"power on", "07E7E"},
+    {"power off", "07E7F"},
+
+    {"input cd", "07A15"},
+    {"input dvd", "07AC1"},
+    {"input bd", "07AC8"},
+    {"input netusb", "0F7F013FC0"},
+    {"input usb",   "0F7F013FC0 0F7F0138C7"},
+    {"input net",   "0F7F013FC0 0F7F0137C8"},
+    {"input pc",    "0F7F013FC0 0F7F0136C9"}, 
+
+    {"volume up", "07A1A"},
+    {"volume down", "07A1B"}, 
+    {"volume -30", "2308B"},
+    {"volume -35", "23081"},
+    {"volume -40", "23077"},
+    {"volume -45", "2306D"},
+    {"volume -50", "23063"},
+    {"volume -55", "23059"},
+    {"volume -60", "2304F"},
+    {"volume -65", "23045"},
+    {"volume -70", "2303B"},
+    {"volume -75", "23031"},
+    {"volume -80", "23027"},
+
+    {"mute on", "07EA2"},
+    {"mute off", "07EA3"},
+
+    {"sleep off", "07EB3"},
+    {"sleep 120", "07EB4"},
+    {"sleep 90", "07EB5"},
+    {"sleep 60", "07EB6"},
+    {"sleep 30", "07EB7"},
+
+    {"dsp 7ch", "07EFF"},
+    {"dsp 2ch", "07EC0"},
+    {"dsp normal", "07EFD"},
+    {"dsp straight", "07EE0"},
+    {"dsp general", "07EFC"},
+
+    {"memory 1", "07E35"},
+    {"memory 2", "07E36"},
+    {"memory 3", "07E37"},
+    {"memory 4", "07E38"},
+    {"memory 5", "07E39"},
+    {"memory 6", "07E3A"},
+
+    {"osd menu", "0F7F012FD0"},
+    {"osd enter", "0F7F0131CE"},
+    {"osd display", "0F7F0135CA"},
+    {"osd up", "0F7F012ED1"},
+    {"osd down", "0F7F0134CB"},
+    {"osd left", "0F7F0132CD"},
+    {"osd right", "0F7F0130CF"},
+
+    {"enhancer on", "07ED8"},
+    {"enhancer off", "07ED9"},
+
+    {"stop", "0F7F013DC2"},
+    {"play", "0F7F013EC1"},
+    {"next", "0F7F013CC3"},
+    {"prev", "0F7F013BC4"},
+
+};
+
